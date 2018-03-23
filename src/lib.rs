e19a36e4e44f07b59ca57accc3d78bbfb48081d0
Vitaly _Vi Shukela <vi0oss@gmail.com>
2018-03-24 02:10:46 +0300

    Change API again and start implementing

--- src/lib.rs
+++ src/lib.rs
@@ -5,35 +5,57 @@
 use std::time::Duration;
 
 /// Natural language to use for the formatting
+/// TODO: connect it to `isolang` crate somehow
 #[allow(missing_docs)]
-#[derive(Debug,Copy,Clone,Eq,PartialEq,Ord,PartialOrd,Hash)]
-pub enum Language {
-    English,
-    Russian,
-    Belarussian,
-    German,
-    Polish,
-    #[doc(hidden)]
-    ThisEnumMayBeExtendedWithoutMajorVersionBump,
+pub trait Language {
+    /// What to emit by default if value is too high
+    fn too_low(&self) -> &'static str;
+    
+    /// What to emit by default if value is too low
+    fn too_high(&self) -> &'static str;
+    
+    /// Chunk of text to put at the end by default
+    fn ago(&self) -> &'static str;
+    
+    /// Get word representing the given time unit, for using with `x` number
+    fn get_word(&self, tu: TimeUnit, x: u64) -> &'static str;
 }
 
-impl Language {
-    fn too_low(&self) -> &'static str {
-        match *self {
-            Language::English => "now",
-            _ => unimplemented!(),
-        }
-    }
-    fn too_high(&self) -> &'static str {
-        match *self {
-            Language::English => "old",
-            _ => unimplemented!(),
-        }
-    }
-    fn ago(&self) -> &'static str {
-        match *self {
-            Language::English => "ago",
-            _ => unimplemented!(),
+/// Default language for timeago
+#[derive(Default)]
+pub struct English;
+impl Language for English {
+    fn too_low (&self) -> &'static str { "now" }
+    fn too_high(&self) -> &'static str { "old" }
+    fn ago(&self)      -> &'static str { "ago" }
+    fn get_word(&self, tu: TimeUnit, x: u64) -> &'static str {
+        use TimeUnit::*;
+        if x == 1 {
+            match tu {
+                Nanoseconds   =>  "nanosecond",
+                Microseconds  =>  "microsecond",
+                Milliseconds  =>  "millisecond",
+                Seconds       =>  "second",
+                Minutes       =>  "minute",
+                Hours         =>  "hour",
+                Days          =>  "day",
+                Weeks         =>  "week",
+                Months        =>  "month",
+                Years         =>  "year",
+            }
+        } else {
+            match tu {
+                Nanoseconds   =>  "nanoseconds",
+                Microseconds  =>  "microseconds",
+                Milliseconds  =>  "milliseconds",
+                Seconds       =>  "seconds",
+                Minutes       =>  "minutes",
+                Hours         =>  "hours",
+                Days          =>  "days",
+                Weeks         =>  "weeks",
+                Months        =>  "months",
+                Years         =>  "years",
+            }
         }
     }
 }
@@ -48,62 +70,120 @@ pub enum TimeUnit {
     Milliseconds,
     Seconds,
     Minutes,
+    Hours,
     Days,
+    Weeks,
     Months,
     Years,
 }
 
-/// Main formatter struct. Build it with new() and maybe modify some options, then use convert
-pub struct Formatter {
-    lang: Language,
-    num_items: usize,
-    min_unit: TimeUnit,
-    max_unit: TimeUnit,
-    too_low: &'static str,
-    too_high: &'static str,
-    ago: &'static str,
-}
-
-impl Formatter {
-    /// Constructor for some default formatting in english
-    pub fn new() -> Formatter {
-        Formatter {
-            lang: Language::English,
-            num_items: 1,
-            min_unit: TimeUnit::Seconds,
-            max_unit: TimeUnit::Years,
-            too_low: Language::English.too_low(),
-            too_high: Language::English.too_high(),
-            ago: Language::English.ago(),
+impl TimeUnit {
+    /// Get `std::time::Duration` corresponding to minimum duration that is representable by this time unit.
+    /// TODO: example
+    pub fn min_duration(&self) -> Duration {
+        use TimeUnit::*;
+        match *self {
+            Nanoseconds => Duration::new(0, 1),
+            Microseconds => Duration::new(0, 1000),
+            Milliseconds => Duration::new(0, 1000_000),
+            Seconds => Duration::new(1, 0),
+            Minutes => Duration::new(60,0),
+            Hours => Duration::new(60*60, 0),
+            Days => Duration::new(24*60*60, 0),
+            Weeks => Duration::new(7*24*60*60, 0),
+            Months => Duration::new(S_IN_MNTH, 0),
+            Years => Duration::new(S_IN_MNTH * 12, 0),
         }
     }
     
-    /// Set formatting language from a pre-defined set.
-    /// Also sets `too_low`, `too_high` and `ago`.
-    /// TODO: example
-    pub fn language(&mut self, x: Language) -> &mut Self {
-        self.lang = x;
-        self.too_low = x.too_low();
-        self.too_high = x.too_high();
-        self.ago = x.ago();
-        self
+    /// TODO
+    pub fn bigger_unit(&self) -> Option<TimeUnit> {
+        use TimeUnit::*;
+        match *self {
+            Nanoseconds  => Some(Microseconds),
+            Microseconds => Some(Milliseconds),
+            Milliseconds => Some(Seconds     ),
+            Seconds      => Some(Minutes     ),
+            Minutes      => Some(Hours       ),
+            Hours        => Some(Days        ),
+            Days         => Some(Weeks       ),
+            Weeks        => Some(Months      ),
+            Months       => Some(Years       ),
+            Years        => None,
+        }
     }
+    
+    /// TODO
+    pub fn smaller_unit(&self) -> Option<TimeUnit> {
+        use TimeUnit::*;
+        match *self {
+            Nanoseconds  => None,
+            Microseconds => Some(Nanoseconds ),
+            Milliseconds => Some(Microseconds),
+            Seconds      => Some(Milliseconds),
+            Minutes      => Some(Seconds     ),
+            Hours        => Some(Minutes     ),
+            Days         => Some(Hours       ),
+            Weeks        => Some(Days        ),
+            Months       => Some(Weeks       ),
+            Years        => Some(Months      ),
+        }
+    }
+}
+
+/// Main formatter struct. Build it with new() and maybe modify some options, then use convert
+/// ```
+/// let f = timeago::Formatter::new();
+/// let d = std::time::Duration::from_secs(3600);
+/// assert_eq!(f.convert(d), "1 hour ago");
+/// ```
+pub struct Formatter<L : Language = English>  {
+    lang: L,
+    num_items: usize,
+    min_unit: TimeUnit,
+    max_unit: TimeUnit,
+    too_low: Option<&'static str>,
+    too_high: Option<&'static str>,
+    ago: Option<&'static str>,
+}
+
+impl Formatter {
+    /// Constructor for some default formatting in English
+    pub fn new() -> Formatter {
+        Formatter::with_language(English)
+    }
+}
+impl <L:Language> Formatter<L> {
+    /// Constructor for some default formatting with specified language instance
+    pub fn with_language(l: L) -> Self  {
+        Formatter {
+            lang: l,
+            num_items: 1,
+            min_unit: TimeUnit::Seconds,
+            max_unit: TimeUnit::Years,
+            too_low: None,
+            too_high: None,
+            ago: None,
+        }
+    }
+    
     
     /// Set number of time unit items to emit (like 1 item for "1 year" or 3 items for "1 year and 3 months and 17 days")
     /// TODO: example
     pub fn num_items(&mut self, x: usize) -> &mut Self {
+        assert!(x > 0);
         self.num_items = x;
         self
     }
     
-    /// Set maximum used unit. Above that it is "old"
+    /// Set maximum used unit. Above that it just emits "old" (or some analogue).
     /// TODO: example
     pub fn max_unit(&mut self, x: TimeUnit) -> &mut Self {
         self.max_unit = x;
         self
     }
     
-    /// Set minimum used unit. Below that it is "now"
+    /// Set minimum used unit. Durations below minimally representable by that unit emit `too_low` value like "now", or like "0 days" instead of "2 minutes".
     /// TODO: example
     pub fn min_unit(&mut self, x: TimeUnit) -> &mut Self {
         self.min_unit = x;
@@ -111,34 +191,86 @@ impl Formatter {
     }
     
     /// Override what is used instead of "now" for too low units.
-    /// Set it after "lang", or it will be overwritten.
+    /// Setting this to special value `"0"` causes to emit output like "0 days", depending on `min_unit` property.
     /// TODO: example
     pub fn too_low(&mut self, x: &'static str) -> &mut Self {
-        self.too_low = x;
+        self.too_low = Some(x);
         self
     }
     
     /// Override what is used instead of "old" for too high units.
-    /// Set it after "lang", or it will be overwritten.
     /// TODO: example
     pub fn too_high(&mut self, x: &'static str) -> &mut Self {
-        self.too_low = x;
+        self.too_high = Some(x);
         self
     }
     
     /// Override what is used instead of "ago".
     /// Empty string literal `""` is a bit special in the space handling.
-    /// Set it after "lang", or it will be overwritten.
     /// TODO: example
     pub fn ago(&mut self, x: &'static str) -> &mut Self {
-        self.ago = x;
+        self.ago = Some(x);
         self
     }
     
     /// Do the actual conversion. Not implemented currently, use version 0.0.2
     /// TODO: example
-    pub fn convert(&self, _x: Duration) -> String {
-        unimplemented!();
+    pub fn convert(&self, d: Duration) -> String {
+        let dtu = dominant_time_unit(d);
+        let (x, _rem) = split_up(d, dtu);
+        format!("{} {} {}", x, self.lang.get_word(dtu, x), self.lang.ago())
+    }
+}
+
+fn dominant_time_unit(d:Duration) -> TimeUnit {
+    use TimeUnit::*;
+    
+    match d {
+        x if x <= Nanoseconds .min_duration() => Nanoseconds ,
+        x if x <= Microseconds.min_duration() => Microseconds,
+        x if x <= Milliseconds.min_duration() => Milliseconds,
+        x if x <= Seconds     .min_duration() => Seconds     ,
+        x if x <= Minutes     .min_duration() => Minutes     ,
+        x if x <= Hours       .min_duration() => Hours       ,
+        x if x <= Days        .min_duration() => Days        ,
+        x if x <= Weeks       .min_duration() => Weeks       ,
+        x if x <= Months      .min_duration() => Months      ,
+        _ => Years,
+    }
+}
+
+fn divmod64(a: u64, b: u64) -> (u64, u64) {
+    (a / b, a % b)
+}
+fn divmod32(a: u32, b: u32) -> (u32, u32) {
+    (a / b, a % b)
+}
+
+fn split_up(d:Duration, tu: TimeUnit) -> (u64, Duration) {
+    let s = d.as_secs();
+    let n = d.subsec_nanos();
+    
+    let tud = tu.min_duration();
+    let tus = tud.as_secs();
+    let tun = tud.subsec_nanos();
+    
+    assert!((tus == 0) ^ (tun == 0));
+    
+    if tus != 0 {
+        if s == 0 {
+            (0, d)
+        } else {
+            let (c, s2) = divmod64(s, tus);
+            (c, Duration::new(s2, n))
+        }
+    } else {
+        if s == 0 {
+            let (c, n2) = divmod32(n, tun);
+            (c.into(), Duration::new(0, n2))
+        } else {
+            // tricky case: may be overflow
+            unimplemented!()
+        }
     }
 }
 
@@ -248,7 +380,7 @@ pub fn format(d: Duration, style: Style) -> String {
     }
 }
 
-
+/*
 #[cfg(test)]
 mod tests {
     use std::time::Duration;
@@ -324,3 +456,4 @@ mod tests {
         assert_eq!(fmts(dns(1000_000_000)), "31Yea");
     }
 }
+*/
