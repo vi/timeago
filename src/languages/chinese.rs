f8afdd85f516515ec3a7c34195d3354721bd287b
Vitaly _Vi Shukela <vi0oss@gmail.com>
2018-03-26 10:21:34 +0300

    Chinese: Fix minor warning

--- src/languages/chinese.rs
+++ src/languages/chinese.rs
@@ -6,7 +6,7 @@ impl Language for Chinese {
     fn too_low (&self) -> &'static str { "刚刚" }
     fn too_high(&self) -> &'static str { "大于" }
     fn ago(&self)      -> &'static str { "之前" }
-    fn get_word(&self, tu: TimeUnit, x: u64) -> &'static str {
+    fn get_word(&self, tu: TimeUnit, _x: u64) -> &'static str {
         use TimeUnit::*;
         match tu {
             Nanoseconds   =>  "纳秒",
