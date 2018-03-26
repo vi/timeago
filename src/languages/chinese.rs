15073f8019e0016526b6b89bac2949a876d27cdf
blackanger <blackanger.z@gmail.com>
2018-03-26 11:03:59 +0800

    add chinese

---
+++ src/languages/chinese.rs
@@ -0,0 +1,39 @@
+use super::super::{Language, TimeUnit};
+
+#[derive(Default)]
+pub struct Chinese;
+impl Language for Chinese {
+    fn too_low (&self) -> &'static str { "刚刚" }
+    fn too_high(&self) -> &'static str { "大于" }
+    fn ago(&self)      -> &'static str { "之前" }
+    fn get_word(&self, tu: TimeUnit, x: u64) -> &'static str {
+        use TimeUnit::*;
+        if x == 1 {
+            match tu {
+                Nanoseconds   =>  "纳秒",
+                Microseconds  =>  "微秒",
+                Milliseconds  =>  "毫秒",
+                Seconds       =>  "秒",
+                Minutes       =>  "分",
+                Hours         =>  "小时",
+                Days          =>  "天",
+                Weeks         =>  "周",
+                Months        =>  "月",
+                Years         =>  "年",
+            }
+        } else {
+            match tu {
+                Nanoseconds   =>  "纳秒",
+                Microseconds  =>  "微秒",
+                Milliseconds  =>  "毫秒",
+                Seconds       =>  "秒",
+                Minutes       =>  "分",
+                Hours         =>  "小时",
+                Days          =>  "天",
+                Weeks         =>  "周",
+                Months        =>  "月",
+                Years         =>  "年",
+            }
+        }
+    }
+}
