322ee9b73b1853dae7d96256e80dbc6eea0224d9
Dragoș Tiselice <dragostiselice@gmail.com>
2018-03-26 14:39:07 +0200

    Added Romanian.

---
+++ src/languages/romanian.rs
@@ -0,0 +1,48 @@
+use super::super::{Language, TimeUnit};
+
+#[derive(Default)]
+pub struct Romanian;
+impl Language for Romanian {
+    fn too_low (&self) -> &'static str { "acum" }
+    fn too_high(&self) -> &'static str { "demult" }
+    fn ago(&self)      -> &'static str { "acum" }
+    fn get_word(&self, tu: TimeUnit, x: u64) -> &'static str {
+        use TimeUnit::*;
+        if x == 1 {
+            match tu {
+                Nanoseconds   =>  "nanosecundă",
+                Microseconds  =>  "microsecundă",
+                Milliseconds  =>  "milisecundă",
+                Seconds       =>  "secundă",
+                Minutes       =>  "minut",
+                Hours         =>  "oră",
+                Days          =>  "zi",
+                Weeks         =>  "săptămână",
+                Months        =>  "lună",
+                Years         =>  "an",
+            }
+        } else {
+            match tu {
+                Nanoseconds   =>  "nanosecunde",
+                Microseconds  =>  "microsecunde",
+                Milliseconds  =>  "milisecunde",
+                Seconds       =>  "secunde",
+                Minutes       =>  "minute",
+                Hours         =>  "ore",
+                Days          =>  "zile",
+                Weeks         =>  "săptămâni",
+                Months        =>  "luni",
+                Years         =>  "ani",
+            }
+        }
+    }
+    fn place_ago_before(&self) -> bool { true }
+}
+
+#[test]
+fn test() {
+    use super::super::Formatter;
+    use std::time::Duration;
+    let f = Formatter::with_language(Romanian);
+    assert_eq!(f.convert(Duration::from_secs(60)), "acum 1 minut");
+}
