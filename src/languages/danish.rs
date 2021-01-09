11e3930d39d204ba7ce3a0c0dc4d19ae81114ffa
Casper Rogild Storm <casper+github@rogildstorm.com>
2021-01-09 16:30:03 +0100

    Create danish.rs

---
+++ src/languages/danish.rs
@@ -0,0 +1,39 @@
+use super::super::{Language, TimeUnit};
+
+#[derive(Default)]
+pub struct Danish;
+impl Language for Danish {
+    fn too_low (&self) -> &'static str { "nu" }
+    fn too_high(&self) -> &'static str { "gammel" }
+    fn ago(&self)      -> &'static str { "siden" }
+    fn get_word(&self, tu: TimeUnit, x: u64) -> &'static str {
+        use TimeUnit::*;
+        if x == 1 {
+            match tu {
+                Nanoseconds   =>  "nanosekund",
+                Microseconds  =>  "mikrosekund",
+                Milliseconds  =>  "millisekund",
+                Seconds       =>  "sekund",
+                Minutes       =>  "minut",
+                Hours         =>  "time",
+                Days          =>  "dag",
+                Weeks         =>  "uge",
+                Months        =>  "måned",
+                Years         =>  "år",
+            }
+        } else {
+            match tu {
+                Nanoseconds   =>  "nanosekunder",
+                Microseconds  =>  "mikrosekunder",
+                Milliseconds  =>  "millisekunder",
+                Seconds       =>  "sekunder",
+                Minutes       =>  "minutter",
+                Hours         =>  "timer",
+                Days          =>  "dage",
+                Weeks         =>  "uger",
+                Months        =>  "måneder",
+                Years         =>  "år",
+            }
+        }
+    }
+}
